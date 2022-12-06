4ff30d984d43a2512b84722085f0a1cb0e2d517d
brunomdr <brunomdr46@gmail.com>
2022-12-06 17:26:55 -0300

    Spring Pet Clinic - Refactor Services to Common Interface

--- SFGPetClinic/pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/services/PetService.java
+++ SFGPetClinic/pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/services/PetService.java
@@ -1,12 +1,6 @@
 package bruno.springframework.sfgpetclinic.services;
 
-
 import bruno.springframework.sfgpetclinic.models.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- SFGPetClinic/pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/services/VetService.java
+++ SFGPetClinic/pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/services/VetService.java
@@ -2,10 +2,5 @@ package bruno.springframework.sfgpetclinic.services;
 
 import bruno.springframework.sfgpetclinic.models.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
