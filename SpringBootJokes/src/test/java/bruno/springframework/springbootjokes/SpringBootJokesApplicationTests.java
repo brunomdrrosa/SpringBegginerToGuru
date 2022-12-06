d96c287bae5fa3bccdf976ee87dcd73aaa0dff2b
brunomdr <brunomdr46@gmail.com>
2022-12-06 15:43:56 -0300

    Creating the Spring Boot Project

---
+++ SpringBootJokes/src/test/java/bruno/springframework/springbootjokes/SpringBootJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package bruno.springframework.springbootjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
