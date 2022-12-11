897f8f45cdd421b09817cfebb1ff45d63814cdfe
brunomdr <brunomdr46@gmail.com>
2022-12-11 17:33:52 -0300

    Spring Pet Clinic - Create Specialty Map Service

---
+++ SFGPetClinic/pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package bruno.springframework.sfgpetclinic.services;
+
+import bruno.springframework.sfgpetclinic.models.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+}
